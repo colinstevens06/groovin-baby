cec193457af100ac12b583407b18826185c49a0c
Colin Stevens <colinstevens06@gmail.com>
2021-04-15 17:28:22 -0400

    dropdowns working

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ $(document).ready(function () {
     $("#" + tab_id).addClass('current');
   })
 
+  $('#deals__completed-order-btn').on("click", function () {
+    $('#deals__completed-order-options').fadeToggle()
+  })
+
+  $('#deals__refunded-order-btn').on("click", function () {
+    $('#deals__refunded-order-options').fadeToggle()
+  })
+
 })
